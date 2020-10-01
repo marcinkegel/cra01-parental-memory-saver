f6b4f68215360f813955e901783186643f182cb4
marcinkegel <kegelmarcin@gmail.com>
2020-10-01 14:16:45 +0200

    memory display carousel added

--- src/js/DisplayMemory.js
+++ src/js/DisplayMemory.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
+import Carousel from "react-elastic-carousel";
 
 
 const DisplayMemory = ({events}) => {
     const [currentMonth, setCurrentMonth] = useState();
     const [currentYear, setCurrentYear] = useState();
-    // const [filteredEvents, setFilteredEvents] = useState();
 
     useEffect(()=>{
         setCurrentMonth(prevState => {
@@ -15,56 +15,52 @@ const DisplayMemory = ({events}) => {
             let date = new Date();
             return date.getFullYear()
         })
-        // setFilteredEvents(prevState => {
-        //
-        // })
+
     },[])
 
-    const displayNext = (e)=>{
+    let displayedMemories = ()=>{
 
-        console.log(e)
+        let filtered = events.filter(event=>{
+            let eventMonth = parseInt(event.date.split('.')[1]);
+            let eventYear = parseInt(event.date.split('.')[2]);
+            return ((eventMonth === (currentMonth + 1)) && ((currentYear-eventYear)>0))
+        })
+        let displayed = filtered.map(function(event){
+            let eventYear = parseInt(event.date.split('.')[2]);
+            let yearsAgo = currentYear-eventYear;
+
+            return(
+
+                <div key={event.id} style={{
+                    backgroundColor: "grey",
+                    width: "400px",
+                    height: "300px"
+                }}
+
+                >
+
+                    <h2> This month  {(yearsAgo===1)? 'last year' : (yearsAgo + ' years ago')}</h2>
+                    <h3 >{event.title}</h3>
+                    <h4 >{event.type}</h4>
+                    <p>{event.note}</p>
+
+                </div>
+
+
+            )
+
+        },0)
+        return displayed
     }
-    const displayPrevious = (e)=>{
-        e.target.parentElement.parentElement.className = 'hide'
-        e.target.parentElement.parentElement.nextSibling.className = 'show'
-    }
-
-
-
-
-
 
     return (
 
 
         <>
-
             <h3>Display Memory</h3>
-            {events.map(function(event, index){
-                let eventMonth = parseInt(event.date.split('.')[1]);
-                let eventYear = parseInt(event.date.split('.')[2]);
-
-                return(
-                    ((eventMonth === (currentMonth + 1)) && ((currentYear-eventYear)>0)) &&
-                        <div key={event.id}
-                             // className={(index===0)? "display": "hide"}
-                            >
-
-                            <h2> This month  {((currentYear-eventYear)===1)? 'last year' : (currentYear-eventYear + ' years ago')}</h2>
-                            <h3 >{event.title}</h3>
-                            <h4 >{event.type}</h4>
-                            <p>{event.note}</p>
-                            <div>
-                                <button onClick={e=>displayPrevious(e)}>previous</button>
-                                <button onClick={e=>displayNext(e)}>next</button>
-                            </div>
-                        </div>
-
-
-                )
-
-            },0)
-            }
+            <Carousel>
+                {displayedMemories()}
+            </Carousel>
         </>
     );
 
